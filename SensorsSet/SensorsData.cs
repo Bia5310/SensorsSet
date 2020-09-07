5506d80a420a8196b14439f61d6b4485bc0ad7db
Ivan Balandin <bia5310@yandex.ru>
2020-09-07 20:51:36 +0300

    Добавьте файлы проекта.

---
+++ SensorsSet/SensorsData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SensorsSet
+{
+    public class SensorsData
+    {
+        public double Humidity = 0;
+        public double Temperature = 0;
+        public double Longitude = 0;
+        public double Latitude = 0;
+        public double Lux = 0;
+        public uint TimeOfWeek = 0;
+        public int Weeks = 0;
+    }
+}
---
+++ SensorsSet_Test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Management;
+using SensorsSet;
+
+namespace SensorsSet_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sensors set = new Sensors();
+            Console.WriteLine(set.AutoConnect(19200));
+            
+            while(true)
+            {
+                try
+                {
+                    var sensorsData = set.ReadSensorsData(5000);
+                    Console.WriteLine("----------------D-A-T-A---------------------");
+                    Console.WriteLine(sensorsData.Humidity.ToString("F2") + "%");
+                    Console.WriteLine(sensorsData.Temperature.ToString("F2") + "°C");
+                    Console.WriteLine(sensorsData.Lux + " lux");
+                    Console.WriteLine(sensorsData.Longitude + "°");
+                    Console.WriteLine(sensorsData.Latitude + "°");
+                    Console.WriteLine(sensorsData.TimeOfWeek + " ms");
+                    Console.WriteLine(sensorsData.Weeks + " неделя с 5-6 января 1980");
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                var keyinfo = Console.ReadKey();
+                if (keyinfo.Key == ConsoleKey.Escape)
+                    return;
+            }
+        }
+    }
+}
